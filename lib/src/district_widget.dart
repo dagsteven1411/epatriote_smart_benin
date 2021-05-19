6cc038bfab338090f4d9243b36903309da4b6f0c
Dagsteven <dagstevengit@gmail.com>
2021-05-19 19:04:19 +0100

    add a stf widget

---
+++ lib/src/district_widget.dart
@@ -0,0 +1,13 @@
+class District_widget extends StatefulWidget {
+  @override
+  _District_widgetState createState() => _District_widgetState();
+}
+
+class _District_widgetState extends State<District_widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
